b8465c8106004d0e5be52bb7253d3979a5dd58b1
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2023-12-31 19:46:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let caracteres = [];
-function codage(code, cle, listecle) {
+
+function codage(code, cle) {
     // Effectuer le codage
     // Votre logique de codage ici
 
@@ -8,21 +9,23 @@ function codage(code, cle, listecle) {
         console.log('Veuillez fournir la clé en ligne de commande.');
         process.exit(1); // Quitter le processus avec un code d'erreur
     }
-    listecle = cle.split('-');
-    // premier rotor
-    return listecle;
-}
 
-function getCodeASCII(liste, cle, listecle) {
+    // La variable listecle est déjà disponible ici, pas besoin de la réassigner
+    // Premier rotor
+    
+}
+function getCodeASCII(liste, cle) {
     if (!liste) {
         console.log("Aucun élément fourni.");
     } else {
+        let code = [];
         for (let i = 0; i < liste.length; i++) {
-            const code = liste[i].charCodeAt(0);
+            code = liste[i].charCodeAt(0);
             caracteres.push(code - 32);
-            // Appeler la fonction de codage avec le code ASCII et la clé
-            codage(code, cle, listecle);
         }
+        // Appeler la fonction de codage avec le code ASCII et la clé
+        codage(code, cle);
+        
     }
 }
 
@@ -34,8 +37,9 @@ const rl = readline.createInterface({
 });
 
 rl.question('Quel message ? ', (message) => {
-    let listecle = [];
-    const cle = process.argv[2];
-    console.log(getCodeASCII(message, cle,listecle));
-    rl.close();
+    const cle = rl.question('Quelle est la clé ? ', (cle) => {
+        listecle = cle.split('-')
+        getCodeASCII(message, cle);
+        rl.close();
+    });
 });
