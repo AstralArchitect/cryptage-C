e3b9dd056b2cf427576fd01c8d591d5b08c5afa9
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2023-12-31 20:09:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 let caracteres = [];
 
 function codage(code, cle) {
-    // Effectuer le codage
-    // Votre logique de codage ici
-
     // Vérifier s'il y a au moins un argument
     if (cle.length === 0) {
-        console.log('Veuillez fournir la clé en ligne de commande.');
+        console.log('Veuillez fournir une clé.');
         process.exit(1); // Quitter le processus avec un code d'erreur
     }
     let codé = code;
     for(let i = 0;i < code.length; i++){
         // Premier rotor
         codé[i] += listecle[0];
-        listecle[0] += 27;
+        listecle[0] += 24;
         // Deuxième rotor
         if(listecle[0] > (255 - 23)) {
             listecle[0] = 1
-            listecle[1] += 1
+            listecle[1] += 24
         }
+        codé[i] -= listecle[1];
+        listecle[1] += 24;
+        // Troisième rotor
+        if(listecle[1] > (255 - 23)) {
+            listecle[1] = 1
+            listecle[2] += 24
+        }
+        codé[i] -= listecle[1];
+        listecle[1] += 24;
     }
 }
 function getCodeASCII(liste, cle) {
