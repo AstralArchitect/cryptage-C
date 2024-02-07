68a4c20ed24fd4cbb998f3b0001291d8a4728394
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-02-07 19:18:23 +0100

    update

--- Code-Source/makefile.mk
+++ Code-Source/makefile.mk
@@ -1,14 +1,21 @@
 # Makefile
 
 # Commande pour exécuter pkg
-PKG = pkg --targets node14-win-x64,node14-macos-x64,node14-linux-x64,node14-linux-arm64,node14-macos-arm64 .
-
+PKG = pkg --targets node14-win-x64,node14-macos-x64,node14-linux-x64 .
+MKDIR = mkdir .\compilé
+MOVE = move cryptage-win-x64.exe .\compilé
+MOVE2 = move cryptage-linux-x64 .\compilé
+MOVE3 = move cryptage-macos-x64 .\compilé
 # Règle par défaut
 all: package
 
 # Règle pour exécuter pkg
 package:
     $(PKG)
+    $(MKDIR)
+    $(MOVE)
+    $(MOVE2)
+    $(MOVE3)
 
 # Nettoyage des fichiers temporaires
 clean:
